c448700a94dc26bb733c9878abf251dbef5eae41
mateussiilva <mateuspython3010@gmail.com>
2024-02-03 21:22:47 -0300

    Create: Cadastro de clientes criado

--- app.py
+++ app.py
@@ -1,39 +1,45 @@
-from flask import Flask, render_template,request
+from flask import Flask, render_template, request, redirect,url_for
 from teste import get_for_id
 
 
 app = Flask(__name__)
 
 
-
 clientes = [
-    {"id":1,"nome":"Mateus jose da silva","idade":23},
+    {"id": 1, "nome": "Mateus jose da silva", "idade": 23},
 
 ]
 
-@app.route("/",methods=["GET"])
+
+@app.route("/", methods=["GET"])
 def index():
     return render_template("index.html", clientes=clientes)
 
-@app.route("/cadastrar",methods=["GET","POST"])
+
+@app.route("/cadastrar", methods=["GET", "POST"])
 def cadastrar():
     if request.method == "GET":
         return render_template("form.html")
+    
     elif request.method == "POST":
         nome = request.form["nomeDoCliente"]
         idade = request.form["idadeDoCliente"]
-        
+
         return f"{nome}{idade}"
+
+
 @app.route("/edit/<int:id>")
 def edit(id):
     cliente = get_for_id(id)
     return f"EDITAND : {cliente}"
 
 
+
 @app.route("/delete/<int:id>")
 def delete(id):
-    
-    return "Delete"
+    return redirect(url_for("index"))
+
+
 
 if __name__ == '__main__':
     # app.run(debug=True,host="0.0.0.0")
